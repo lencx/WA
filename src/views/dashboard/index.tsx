fa94c4c864a70af0f2711dc08986fb05ff5de6f0
xiaofuyesnew <xiaofuyesnew@qq.com>
2022-11-18 16:54:54 +0800

    fix: dashboard icons not appear

--- src/views/dashboard/index.tsx
+++ src/views/dashboard/index.tsx
@@ -11,22 +11,22 @@ export default function DashboardView() {
   return (
     <Layout className="dashboard">
       <Error type="WA+ Setting" data={settingJSON} shortcut="CmdOrCtrl + ," />
-      {settingJSON?.app?.map((group: any, idx: number) => {
-          return (
-            <div className="wa-app" key={`${group.type}_${idx}`}>
-              {group.type && <h3>{group.type}</h3>}
-              <div className="wa-app-group">
-                {group?.items?.map((app: AppData) => (
-                  <AppItem
-                    key={app.name}
-                    data={app}
-                    type={group.type}
-                  />
-                ))}
-              </div>
+      {settingJSON?.map((group: any, idx: number) => {
+        return (
+          <div className="wa-app" key={`${group.type}_${idx}`}>
+            {group.type && <h3>{group.type}</h3>}
+            <div className="wa-app-group">
+              {group?.items?.map((app: AppData) => (
+                <AppItem
+                  key={app.name}
+                  data={app}
+                  type={group.type}
+                />
+              ))}
             </div>
-          )
-        })
+          </div>
+        )
+      })
       }
     </Layout>
   )
